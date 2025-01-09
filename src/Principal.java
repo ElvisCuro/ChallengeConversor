f2d867b088a487f0fcbedefddbad5e4ce6d106ef
ElvisCuro <elviscuro0@gmail.com>
2025-01-09 18:34:08 -0500

    create class ConvertirMoneda

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,8 @@
+
 public class Principal {
     public static void main(String[] args) {
-
+        ConsultaMoneda consulta = new ConsultaMoneda();
+        Moneda moneda = consulta.buscaMoneda("USD","PEN");
+        System.out.println(moneda);
     }
 }
